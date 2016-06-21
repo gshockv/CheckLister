5b5334df068643a30b43a40f0351379f5a0fd1b4
Vitaly Gashock <gshockv@gmail.com>
2016-06-22 00:45:00 +0300

    initial dummy tableView

---
+++ CheckLister/ChecklistViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CheckLister
+//
+//  Created by Vitaly Gashock on 6/21/16.
+//  Copyright © 2016 Vitaly Gashock. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS Development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Learn FIFA 16"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat Ice Cream Sandwich"
+        }
+        
+        return cell
+    }
+}
+
